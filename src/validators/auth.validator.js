b9b7af2d3afd2df85c5a3f95beb72702b9f1f826
Code_NooB <alexangel77719@gmail.com>
2026-06-16 00:02:46 +0300

    add auth validators for register and login

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,16 @@
+import { Joi, Segments, celebrate } from 'celebrate'
+
+export const registerValidator = celebrate({
+  [Segments.BODY]: Joi.object({
+    username: Joi.string().min(3).max(30).required(),
+    password: Joi.string().min(6).required(),
+    name: Joi.string().min(2).required(),
+  }),
+})
+
+export const loginValidator = celebrate({
+  [Segments.BODY]: Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+})
